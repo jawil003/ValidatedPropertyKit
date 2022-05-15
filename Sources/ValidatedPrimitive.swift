b33882e3047aa743ab164ba01349ff807f55444d
Jannik Will <jw19980309@gmail.com>
2022-05-15 21:03:46 +0200

    Update

---
+++ Sources/ValidatedPrimitive.swift
@@ -0,0 +1,46 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jannik Will on 15.05.22.
+//
+
+import Foundation
+import Combine
+
+@propertyWrapper
+struct ValidatedPrimitive<Value> {
+    
+    public var wrappedValue: Value {
+        didSet {
+            self.isValid = self.validate(value: self.wrappedValue)
+        }
+    }
+    public var isValid: Bool
+    
+    private var validations: [Validation<Value>] = []
+    var errorValidations: [Validation<Value>] = []
+    
+    init(_ wrappedValue: Value, validations: [Validation<Value>]) {
+        self.wrappedValue = wrappedValue
+        self.validations = validations
+        self.isValid = true
+        self.isValid = validate(value: wrappedValue)
+    }
+    
+    mutating func validate(value: Value) -> Bool {
+        var valid = true
+        
+        for v in validations {
+            let localValid = v.isValid(value: value)
+            
+            if !localValid {
+                errorValidations.append(v)
+            }
+            
+            valid = localValid
+        }
+        
+        return valid
+    }
+}
