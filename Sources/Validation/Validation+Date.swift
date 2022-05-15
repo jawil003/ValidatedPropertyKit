fc8ef28fc94bd0d27dc2a0b37964a35224c5d940
Jannik Will <jw19980309@gmail.com>
2022-05-15 15:49:39 +0200

    Update Date Comparisons

---
+++ Sources/Validation/Validation+Date.swift
@@ -0,0 +1,52 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jannik Will on 15.05.22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Validation where Value == Date {
+    
+    static func isFuture(message: LocalizedStringKey? = nil) -> Self {
+        .init(predicate: { value in
+            value > Date()
+        }, message: message)
+    }
+    
+    static func isFutureDate(message: LocalizedStringKey? = nil) -> Self {
+        .init(predicate: { value in
+            let order = Calendar.current.compare(value, to: Date(), toGranularity: .day)
+            return order == .orderedAscending
+        }, message: message)
+    }
+    
+    static func isPast(message: LocalizedStringKey? = nil) -> Self {
+        .init(predicate: { value in
+            value < Date()
+        }, message: message)
+    }
+    
+    static func isPastDate(message: LocalizedStringKey? = nil) -> Self {
+        .init(predicate: { value in
+            let order = Calendar.current.compare(value, to: Date(), toGranularity: .day)
+            return order == .orderedDescending
+        }, message: message)
+    }
+    
+    static func isCurrent(message: LocalizedStringKey? = nil) -> Self {
+        .init(predicate: { value in
+            value == Date()
+        }, message: message)
+    }
+    
+    static func isCurrentDate(message: LocalizedStringKey? = nil) -> Self {
+        .init(predicate: { value in
+            let order = Calendar.current.compare(value, to: Date(), toGranularity: .day)
+            return order == .orderedSame
+        }, message: message)
+    }
+    
+}
